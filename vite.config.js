d4b0cd2fa3bf3818ed5f75b0205f67c0f50566a0
ronaldrosil <madmuscnatura@gmail.com>
2026-04-04 15:51:14 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-node_modules
-dist
-.env
-.env.local
-.DS_Store
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
